2cbf7c9d93708267f197230a0a41491ccee5f567
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-02-29 17:55:20 -0300

    Criação do repository e model

--- src/main/java/br/com/springboot/cursojdevtreinamento/controller/UserController.java
+++ src/main/java/br/com/springboot/cursojdevtreinamento/controller/UserController.java
@@ -1,6 +1,9 @@
 package br.com.springboot.cursojdevtreinamento.controller;
 
 
+import br.com.springboot.cursojdevtreinamento.model.UsuarioModel;
+import br.com.springboot.cursojdevtreinamento.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String usuarioText(@PathVariable String name){
@@ -20,6 +26,11 @@ public class UserController {
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome){
+
+        UsuarioModel usuario = new UsuarioModel();
+        usuario.setNome(nome);
+        usuarioRepository.save(usuario);//Este save vai gravar no banco de dados
+
         return "Olá Mundo " + nome;
     }
 }
